1cb4143f04785b4140a6d1d27c5294a1344032ec
Antoni Zajko <antoni.zajko.1@gmail.com>
2022-03-30 21:24:04 +0200

    Add processed data downloading script

---
+++ bin/download_processed.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget -O data/data_with_altmetric.csv 'https://www.dropbox.com/s/4v9dax9vwszqxu3/data_with_altmetric.csv'
+wget -O data/responses.pkl 'https://www.dropbox.com/s/dykwy1hvzxxazhl/responses.pkl'
+wget -O data/samples.csv 'https://www.dropbox.com/s/v54gv0el854leps/sampled.csv'
